8f92892c0a192f1135ede1c081d24ed9686d7aa2
fisherjk <fisherjk@.uwec.edu>
2014-02-24 14:05:47 -0600

    First Commit
    Signed-off-by: fisherjk <fisherjk@.uwec.edu>

--- src/fisherjk/Hello.java
+++ src/fisherjk/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 			System.out.println("Hello World in file");
 			System.out.println("https://github.com/fisherjk/HelloWorld.git");
-	}
+			System.out.println("Testing again");
+		}
 
 }
