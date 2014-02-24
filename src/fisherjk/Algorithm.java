f5c1153a0baa50e290013e629cfe22596bb35409
fisherjk <fisherjk@.uwec.edu>
2014-02-24 14:22:46 -0600

    Starting to work on implementing algorithm
    Signed-off-by: fisherjk <fisherjk@.uwec.edu>

---
+++ src/fisherjk/Algorithm.java
@@ -0,0 +1,6 @@
+package fisherjk;
+
+public class Algorithm {
+
+
+}
---
+++ src/fisherjk/AlgorithmMain.java
@@ -0,0 +1,10 @@
+package fisherjk;
+
+public class AlgorithmMain {
+ 
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
